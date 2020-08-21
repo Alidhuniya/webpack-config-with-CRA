f29e97c83b2ae0d2950128d605942c03acb99629
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-08-21 10:00:31 -0700

    css setup config added

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>Hello World from Webpack</h1>
+      <h1 className = "heading">Hello World from Webpack</h1>
       <h3>h3</h3>
-      <h5></h5>
+      <h5>Wala</h5>
     </div>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,10 @@ module.exports = {
         ]
       }
     },
-  
+    {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    },
   ]
   },
   // resolve: {
